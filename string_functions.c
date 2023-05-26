ee585eebf0bc9e4427d2e0a34419b274b1c6f190
Uzom12345 <idumauzoma@yahoo.com>
2023-05-26 08:09:54 -0700

    updated

---
+++ string_functions.c
@@ -0,0 +1,75 @@
+#include "shell.h"
+
+/**
+ * _strlen - this returns the length of a string
+ * @s: the string whose length to check
+ *
+ * Return: this is an integer length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * _strcmp - performs lexicogarphic comparison of two strangs.
+ * @s1: this is the first strang
+ * @s2: this is the second strang
+ *
+ * Return: it is negative if s1 < s2, positive if s1 > s2, and also zero if s1 == s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+		return (0);
+	else
+		return (*s1 < *s2 ? -1 : 1);
+}
+
+/**
+ * starts_with - it help check if needle starts with haystack
+ * @haystack: it help string to search
+ * @needle: it help the substring to find
+ *
+ * Return: address of next char of haystack or NULL
+ */
+char *starts_with(const char *haystack, const char *needle)
+{
+	while (*needle)
+		if (*needle++ != *haystack++)
+			return (NULL);
+	return ((char *)haystack);
+}
+
+/**
+ * _strcat - this concatenates two strings
+ * @dest: the destination buffer
+ * @src: this is the buffer source
+ *
+ * Return: pointer to destination buffer
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *ret = dest;
+
+	while (*dest)
+		dest++;
+	while (*src)
+		*dest++ = *src++;
+	*dest = *src;
+	return (ret);
+}
